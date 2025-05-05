4c7fd97729dbdeab891338afea2dc1b26617686d
LookItsCashew <58984123+LookItsCashew@users.noreply.github.com>
2025-05-05 08:10:04 -0700

    Write out the generated SQL statements to textbox

--- TSQL/TransactSQL.cs
+++ TSQL/TransactSQL.cs
@@ -1,8 +1,31 @@
-﻿namespace CustomerImportSQL.TSQL;
+﻿using System.Text;
+
+namespace CustomerImportSQL.TSQL;
 
 public enum TransactSQLType
 {
     UPDATE,
     DELETE,
     INSERT
+}
+
+public static class TransactSQLExport
+{
+    public static void WriteTSQLToTextBox(List<TransactSQLModel> models, TextBox textBox)
+    {
+        string[] statements = new string[models.Count];
+        try
+        {
+            for (int i = 0; i < models.Count; i++)
+            {
+                statements[i] = models[i].TransactStatement;
+            }
+
+            textBox.Lines = statements;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
 }
