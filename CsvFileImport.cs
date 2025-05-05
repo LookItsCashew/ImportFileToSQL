01658b52a44b778ec748588f8479c4ba7a3806de
LookItsCashew <58984123+LookItsCashew@users.noreply.github.com>
2025-05-05 08:09:30 -0700

    removed unused method
    intended this to be a more efficient version of csv file parsing, but this was not the bottleneck of the app.

--- CsvFileImport.cs
+++ CsvFileImport.cs
@@ -10,7 +10,7 @@ public static class CsvFileImport
     /// Opens a CSV file at a given filepath and reads its contents into a 
     /// data table.
     /// </summary>
-    /// <param name="csvFilePath">the selected file from an open file dialog</param>
+    /// <param name="csvFilePath">The selected file from an open file dialog</param>
     /// <returns>DataTable with columns and rows read from CSV file</returns>
     public static async Task<DataTable> GetDataViewFromCsvFileAsync(string csvFilePath)
     {
@@ -36,47 +36,6 @@ public static class CsvFileImport
 
             dt.Rows.Add(row);
         }
-        return dt;
-    }
-    
-    // TODO: get this working. goal is to optimize file reading for fast data retrieval.
-    public static async Task<DataTable> GetDataFromCsvFileAsync(string csvFilePath)
-    {
-        Stopwatch  stopwatch = new Stopwatch();
-        stopwatch.Start();
-        DataTable dt = new DataTable();
-        using StreamReader sr = new StreamReader(csvFilePath);
-
-        try
-        {
-            // get the column headers from the file
-            string? colHeaders = await sr.ReadLineAsync();
-            string[] headers = colHeaders!.Split(',');
-            
-            // create columns and add them to the table
-            foreach (string header in headers)
-            {
-                dt.Columns.Add(header);
-            }
-            
-            // advance through each line in file to generate rows
-            while (!sr.EndOfStream)
-            {
-                string? row = await sr.ReadLineAsync();
-                string[] rowCells = row!.Split(',');
-                dt.Rows.Add(rowCells);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        finally
-        {
-            sr.Close();
-        }
-        stopwatch.Stop();
-        Console.WriteLine(@"Original Method Time Elapsed: " + stopwatch.ElapsedMilliseconds);
         return dt;
     }
 }
